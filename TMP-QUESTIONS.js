334876abcebae15326b7106deac4dd80f72091b6
Jon Pointer <jonpointer@msn.com>
2021-03-03 22:21:34 -0700

    Set up initial starter REACT structure and files

---
+++ TMP-QUESTIONS.js
@@ -0,0 +1,17 @@
+/* 
+
+Questions:
+
+- In server.js, confused by " res.sendFile(path.join(__dirname, "./client/build/index.html"));  build directory??"
+
+- And "app.use(express.static("client/build"));"
+
+- Given this, how does it know to use index.html in public
+
+- Why client/public/manifest.json?  In case you want to create a PWA
+
+
+
+
+
+*/
